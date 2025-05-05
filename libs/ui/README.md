0855f0aedeff1718d74a1d03a6065fd72c38bc0f
sabdawork <sabdadeveloper@gmail.com>
2025-05-05 12:02:11 +0800

    ui components

---
+++ libs/ui/README.md
@@ -0,0 +1,7 @@
+# @v3testing/ui
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test @v3testing/ui` to execute the unit tests via [Vitest](https://vitest.dev/).
---
+++ libs/ui/src/lib/ui.tsx
@@ -0,0 +1,10 @@
+export function UiComps() {
+  return (
+    <div >
+      <h1 className="text-red-300">Welcome to Ui!</h1>
+      <button className="btn btn-soft btn-primary">Primary</button>
+    </div>
+  );
+}
+
+export default UiComps;
